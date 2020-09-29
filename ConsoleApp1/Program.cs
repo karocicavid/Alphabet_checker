3e73269e2206c5cf130ed19d051a18f74e7ab4e0
Cavid <cavidmammad@mail.ru>
2020-09-29 20:32:10 +0300

    For checking letters

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1
+{
+ class Program
+    {
+        delegate void Name(string input);
+        static void Main()
+        {
+            Name name = delegate (string input1)
+            {
+                Console.WriteLine("Hi Mr " + input1);
+            };
+            Console.Write("Enter your name :");
+            string x;
+            bool check = true;
+            do
+            {
+                x = Console.ReadLine();
+                foreach (char i in x)
+                {
+                    
+                     if (i < '0' || i > '9') 
+                    {
+                        foreach(char a in "the quick brown fox jumps over the lazy dog THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")
+                        {
+                            if (a == i) { check = true; break; }
+                            else { check = false;}
+                        }
+                    }
+                    else { Console.WriteLine("input again!"); check = false;break; }
+                    if (check == false) { Console.WriteLine("input again!");  break; }
+                }
+            } while (check == false);
+            name(x);
+           
+        }
+    }
+
+
+}
